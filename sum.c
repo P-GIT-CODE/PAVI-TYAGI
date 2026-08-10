725180c698b42ced92115e7d770fd924e531f3e7
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-08-10 15:41:19 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,17 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+
+
+#include<stdio.h>
+int main(){
+    int a, b ;
+    printf("enter value of a:\n");
+    scanf("%d" , &a);
+    printf("enter value of b:\n");
+    scanf("%d" , &b);
+    printf("Sum :%d" , a+b);
+
+    return 0;
+}
+
+  
