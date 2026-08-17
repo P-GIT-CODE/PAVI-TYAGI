8b93e213cfecdfe285a13262905c51adf4655f4f
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-08-18 01:36:48 +0530

    Add files via upload

---
+++ character check.c
@@ -0,0 +1,28 @@
+//Q15: Write a program  in c to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+
+    
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+
+
+    
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("'%c' is alphabet.", ch);
+    }
+    else if(ch >= '0' && ch <= '9')
+    {
+        printf("'%c' is digit.", ch);
+    }
+    else 
+    {
+        printf("'%c' is special character.", ch);
+    }
+
+    return 0;
+}
