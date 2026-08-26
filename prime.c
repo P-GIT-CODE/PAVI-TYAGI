df5576b3c1d3b3aa529164306180ec751012c4af
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-08-26 23:26:03 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,27 @@
+//Q34: Write a program to check if a number is prime.
+
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, rem, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        sum = sum + rem * rem * rem;
+        n = n / 10;
+    }
+
+    if(sum == temp)
+        printf("Armstrong number");
+    else
+        printf("Not an Armstrong number");
+
+    return 0;
+}
