b0e497f74b38f5298ebd94e238d152bcaa015f8c
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-08-26 23:24:49 +0530

    Add files via upload

---
+++ armstrong.c
@@ -0,0 +1,26 @@
+//Q33: Write a program to check if a number is an Armstrong number.
+#include <stdio.h>
+
+int main()
+{
+    int n, temp, rem, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        rem = n % 10;
+        sum = sum + rem * rem * rem;
+        n = n / 10;
+    }
+
+    if(sum == temp)
+        printf("Armstrong number");
+    else
+        printf("Not an Armstrong number");
+
+    return 0;
+}
