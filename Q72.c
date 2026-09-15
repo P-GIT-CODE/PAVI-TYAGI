a41a639be254c0827a44cc6b1b17d830db67f9a7
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-09-15 17:35:08 +0530

    Add files via upload

---
+++ Q72.c
@@ -0,0 +1,34 @@
+/*Q72: Find the sum of all elements in a matrix.
+
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+21
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10];
+    int r, c, i, j, sum = 0;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter matrix elements:\n");
+
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+            sum = sum + a[i][j];
+        }
+    }
+
+    printf("Sum of all elements = %d", sum);
+
+    return 0;
+}
