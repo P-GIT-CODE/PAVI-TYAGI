2b7a3eb60a886e2d48cfb1a1d0d52422cbb16f56
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-09-15 17:35:49 +0530

    Add files via upload

---
+++ Q73.c
@@ -0,0 +1,50 @@
+/*Q73: Find the sum of each row of a matrix and store it in an array.
+
+Sample Test Cases:
+Input 1:
+2 3
+1 2 3
+4 5 6
+Output 1:
+6 15
+
+*/
+#include <stdio.h>
+
+int main()
+{
+    int a[10][10], sum[10];
+    int r, c, i, j;
+
+    printf("Enter number of rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter the elements of the matrix:\n");
+
+    for(i = 0; i < r; i++)
+    {
+        for(j = 0; j < c; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    for(i = 0; i < r; i++)
+    {
+        sum[i] = 0;
+
+        for(j = 0; j < c; j++)
+        {
+            sum[i] = sum[i] + a[i][j];
+        }
+    }
+
+    printf("Sum of each row:\n");
+
+    for(i = 0; i < r; i++)
+    {
+        printf("%d ", sum[i]);
+    }
+
+    return 0;
+}
