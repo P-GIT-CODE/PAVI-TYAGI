cdf2d1a450e246be533df38909c1f9091e7c5aef
P-GIT-CODE <PAVI.32913@stu.upes.ac.in>
2026-09-18 08:49:06 +0530

    Add files via upload

---
+++ Q75.c
@@ -0,0 +1,49 @@
+/*Q75: Add two matrices.
+
+Sample Test Cases:
+Input 1:
+2 2
+1 2
+3 4
+2 2
+5 6
+7 8
+Output 1:
+6 8
+10 12
+
+*/
+#include <stdio.h>
+
+int main() {
+    int a[10][10], b[10][10], sum[10][10];
+    int r, c, i, j;
+
+    printf("Enter rows and columns: ");
+    scanf("%d %d", &r, &c);
+
+    printf("Enter first matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("Enter second matrix:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    printf("Sum of matrices:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+            printf("%d ", sum[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
